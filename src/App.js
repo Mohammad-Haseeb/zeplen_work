0a06bfa674d07421abf4a7a2cea6eab31e6f18e8
Muhammad Haseeb <Mohammad-Haseeb>
2020-11-07 18:45:19 +0500

    Firebase installation

--- src/App.js
+++ src/App.js
@@ -2,8 +2,11 @@ import React from 'react'
 import Signup from './Signup';
 import Login from './Login'
 import { Routes, Route} from 'react-router-dom';
+import firebase from './FirebaseConfig';
 
 function App() {
+  console.log(firebase.database);
+  firebase.database.ref('Student').set("Ali");
     
   return (
       
